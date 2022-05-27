d59ecf36622dfa4f088b3b42f4252164b7540ae3
Ashish <aashish7322@gmail.com>
2022-05-27 19:56:40 +0530

    reverse.sh

---
+++ reverse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+randomcheck1=$((RANDOM%1000))
+randomcheck2=$((RANDOM%1000))
+randomcheck3=$((RANDOM%1000))
+
+
+
+if[$randomcheck1 -gt $randomcheck2 && $randomcheck1 -gt $randomcheck3]
+echo "$randomcheck1 is greatest"
+
+elif[ $randomcheck2 -gt $randomcheck3 ]
+echo "$randomcheck2 is greatest"
+
+else
+echo"$randomcheck3 is greatest"
+
